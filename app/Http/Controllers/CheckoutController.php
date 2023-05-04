e147efea104f0bcc45b6e612c7fcceaeeae611eb
Cesar Martinez <cesarmartinez9311@gmail.com>
2023-05-04 00:34:11 -0600

    validate if copy is available

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CheckoutRequest;
 use App\Models\BookCopy;
+use App\Models\Checkout;
 use App\Services\CheckoutService;
 use Illuminate\Http\JsonResponse;
 
@@ -13,11 +14,20 @@ class CheckoutController extends ApiController
      * Store a newly created resource in storage.
      *
      * @param CheckoutRequest $request
+     * @param BookCopy $copy
      * @return JsonResponse
      */
     public function store(CheckoutRequest $request, BookCopy $copy): JsonResponse
     {
         $data = $request->validated();
+
+        $copy_is_available = !Checkout::query()
+            ->where('book_copy_id',$copy->id)
+            ->where('returned',0)->exists();
+
+        if(!$copy_is_available){
+            return $this->respondBadRequest('The copy is already rented');
+        }
 
         $checkout = CheckoutService::create($copy, $data['days']);
 
