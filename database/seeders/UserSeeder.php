dadcb07aa69b9d8452ad2102e98441341a6fcb53
Cesar Martinez <cesarmartinez9311@gmail.com>
2023-05-07 19:53:35 -0600

    testing

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'first_name' => 'Cesar',
+            'last_name'  => 'Martinez',
+            'email'      => 'cesarmartinez@gmail.com',
+            'password'   => strtolower('password'),
+            'role_id'    => 1
+        ]);
+
+        \App\Models\User::create([
+            'first_name' => 'Testing',
+            'last_name'  => 'User',
+            'email'      => 'testing@gmail.com',
+            'password'   => strtolower('password'),
+            'role_id'    => 1
+        ]);
+
+        \App\Models\User::create([
+            'first_name' => 'Focus',
+            'last_name'  => 'User',
+            'email'      => 'focususer@gmail.com',
+            'password'   => strtolower('password'),
+            'role_id'    => 2
+        ]);
+    }
+}
